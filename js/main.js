c555452cb5a40f789da4e5d01313e2becd243127
aayman997 <ahmed1997164@gmail.com>
2020-05-02 04:55:25 +0200

    Login & Register Done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Side Menu Settings
-const sideMenuHeight = $('.side-menu').height();
 const DomElements = {
 	navBarBtn: document.querySelector('.navbar-toggler'),
 	sideMenuClose: document.querySelector('.side-menu .close-menu button')
@@ -17,3 +16,32 @@ function menuClose() {
 
 DomElements.navBarBtn.addEventListener('click', menuOpen);
 DomElements.sideMenuClose.addEventListener('click', menuClose);
+
+$('.input-group-append').on('click', function () {
+	if ($(this).siblings().attr('type') === 'password') {
+		$(this).siblings().prop('type', 'text');
+	} else if ($(this).siblings().attr('type') === 'text') {
+		$(this).siblings().prop('type', 'password');
+	}
+	$(this).find('i')
+		.toggleClass('fa-eye')
+		.toggleClass('fa-eye-slash');
+});
+
+if (window.location.href.indexOf('home') > -1) {
+	$('.bottom-nav .fa-home').parent('.nav-link')
+		.addClass('active')
+		.siblings().removeClass('active');
+} else if (window.location.href.indexOf('search') > -1) {
+	$('.bottom-nav .fa-search').parent('.nav-link')
+		.addClass('active')
+		.siblings().removeClass('active');
+} else if (window.location.href.indexOf('group') > -1) {
+	$('.bottom-nav .fa-users').parent('.nav-link')
+		.addClass('active')
+		.siblings().removeClass('active');
+} else if (window.location.href.indexOf('profile') > -1) {
+	$('.bottom-nav .fa-user').parent('.nav-link')
+		.addClass('active')
+		.siblings().removeClass('active');
+}
